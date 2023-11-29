73f6ce3ef6befbc12d8c1992ae1c63274f7ed04f
Elijah Schneider <110437948+Jetvac2@users.noreply.github.com>
2023-11-29 10:18:47 -0500

    Added the correct value for the turning gear ratio.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -114,7 +114,7 @@ public final class Constants {
     public static final double kMaxRPM = 5676;
     public static final double kWheelDiameterMeters = 0.1016;
     public static final double kDriveGearRatio = 6.75/1;
-    public static final double kTurningGearRatio = 1/1; // TODO: Change to actully gear ratio
+    public static final double kTurningGearRatio = 150/7:1; 
 
     public static final double kAbsoluteTurningEncoderCPRToDegrees = 
     (kAbsoluteTurningEncoderCPR / kAbsoluteTurningEncoderCPR) * 360;
